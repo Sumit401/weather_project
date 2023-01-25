70360c1e40547f4ad87fcf301cfe731b0a16a36b
Sumit <sumitsinha401@gmail.com>
2023-01-25 20:56:07 +0530

    Initial Commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -17,9 +17,53 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Weather Data")),
-      body:
-          Consumer<WeatherProvider>(builder: (context, weatherProvider, child) {
-        return Column(
+      body: Consumer<WeatherProvider>(
+          builder: (context, weatherProvider, child) {
+        return Container(
+          alignment: Alignment.center,
+          color: Colors.blueAccent,
+          child: Stack(
+            fit: StackFit.expand,
+            alignment: AlignmentDirectional.center,
+            children: [
+              Image.asset("assets/images/background.png", fit: BoxFit.fill),
+              textFormField(),
+              Container(
+                margin: EdgeInsets.only(top: 200,bottom: 200),
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(200)),color: Color.fromARGB(90, 30, 60, 20)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    // Text field with Buttons................
+
+                    Visibility(
+                      visible: weatherProvider.contentVisibility,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            textForTimeZone(weatherProvider),
+                            textForPlaceName(weatherProvider),
+                            textForWeatherDesc(weatherProvider),
+                            textForTemp(weatherProvider),
+                            textForFeelsLike(weatherProvider),
+                            textForVisibility(weatherProvider),
+                            textForHumidity(weatherProvider),
+                            textForPressure(weatherProvider),
+                            textForSunrise(weatherProvider),
+                            textForSunset(weatherProvider),
+                            textForWindSpeed(weatherProvider),
+                            textForCloudData(weatherProvider),
+                          ]
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ],
+          ),
+        );
+
+          /*Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             // Text field with Buttons................
@@ -45,7 +89,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ],
-        );
+        );*/
       }),
     );
   }
